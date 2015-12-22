cb1b79b026c6ab786a242fcdc0dccb8fa8c1eed0
Cat73 <1901803382@qq.com>
2015-12-22 16:17:30 +0800

    更新版本号: 0.3.0

--- src/main/java/org/cat73/xray/reference/Reference.java
+++ src/main/java/org/cat73/xray/reference/Reference.java
@@ -3,7 +3,7 @@ package org.cat73.xray.reference;
 public class Reference {
     public final static String MODID = "Cat-Xray";
     public final static String NAME = "Cat-Xray";
-    public final static String VERSION = "0.2.0";
+    public final static String VERSION = "0.3.0";
     public final static boolean DEBUG = true;
     public final static String PROXY_SERVER = "org.cat73.xray.proxy.ServerProxy";
     public final static String PROXY_CLIENT = "org.cat73.xray.proxy.ClientProxy";
