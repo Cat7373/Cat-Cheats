fea5bf2c75c5f73df312705b1762d298053f6bd6
Cat73 <1901803382@qq.com>
2015-12-24 14:57:57 +0800

    新的pre-稳定版 0.4.0

--- src/main/java/org/cat73/cheats/reference/Reference.java
+++ src/main/java/org/cat73/cheats/reference/Reference.java
@@ -3,7 +3,7 @@ package org.cat73.cheats.reference;
 public class Reference {
     public final static String MODID = "Cat-Cheats";
     public final static String NAME = "Cat-Cheats";
-    public final static String VERSION = "0.3.0";
+    public final static String VERSION = "0.4.0";
     public final static boolean DEBUG = true;
     public final static String PROXY_SERVER = "org.cat73.cheats.proxy.ServerProxy";
     public final static String PROXY_CLIENT = "org.cat73.cheats.proxy.ClientProxy";
