dba8741c9abb50655f0ffc6dec7f0516b22f3614
Cat73 <1901803382@qq.com>
2016-01-18 12:27:43 +0800

    更改 build 脚本; 新的稳定版0.4.2

--- src/org/cat73/cheats/reference/Reference.java
+++ src/org/cat73/cheats/reference/Reference.java
@@ -3,6 +3,7 @@ package org.cat73.cheats.reference;
 public class Reference {
     public final static String MODID = "Cat-Cheats";
     public final static String NAME = "Cat-Cheats";
+    public final static String VERSION = "${version}";
     public final static boolean DEBUG = true;
     public final static String PROXY_CLIENT = "org.cat73.cheats.proxy.ClientProxy";
     public final static String PROXY_SERVER = "org.cat73.cheats.proxy.ServerProxy";
